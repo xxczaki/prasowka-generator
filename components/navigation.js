08bd8d23b5e1ba609599de355bf67ee0e2db5d3f
Antoni Kepinski <xxczaki@pm.me>
2019-10-06 00:46:39 +0200

    Add FAQ page to navbar

--- components/navigation.js
+++ components/navigation.js
@@ -3,6 +3,6 @@ import Link from 'next/link';
 
 import ExternalLink from './link';
 
-const Navigation = () => <p style={{margin: '2px', paddingBottom: '1.5em', paddingTop: '1em'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></p>;
+const Navigation = () => <p style={{margin: '2px', paddingBottom: '1.5em', paddingTop: '1em'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/faq"><ExternalLink>[FAQ]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></p>;
 
 export default Navigation;
