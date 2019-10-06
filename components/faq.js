46bf7db6b3e43c0787cbceca4a28b987dd4adb95
Antoni Kepinski <xxczaki@pm.me>
2019-10-06 21:31:27 +0200

    Change FAQ questions

--- components/faq.js
+++ components/faq.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Faq = () => (
 	<>
 		<details>
-			<summary>Czy aby dostać 6 wystarczy wygenerować prasówkę?</summary>
+			<summary>Czy aby dostać dobrą ocenę wystarczy wygenerować prasówkę?</summary>
 			<p>Nie. Generator został stworzony, by pomóc uczniom w tworzeniu prasówek.</p>
 			<p>Nadal musisz się ich nauczyć, wyszukać dodatkowe informacje o temacie itp.</p>
 		</details>
 		<br/>
 		<details>
 			<summary>Skąd pochodzą wiadomości?</summary>
-			<p>Po znalezieniu wiadomości na dole strony znajduje się źródło.</p>
+			<p>Po wygenerowaniu wiadomości na dole strony znajduje się ich źródło.</p>
 		</details>
 		<br/>
 		<details>
