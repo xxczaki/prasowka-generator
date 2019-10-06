bf1f8932d77ce16f55a8e80b6ee6e0203ba4f447
Antoni Kepinski <xxczaki@pm.me>
2019-10-06 22:48:25 +0200

    Only 1 button

--- components/generator.js
+++ components/generator.js
@@ -1,117 +1,34 @@
 import React, {useState} from 'react';
-import {useFormState} from 'react-use-form-state';
 
-import Select from './select';
 import Button from './button';
-import ExternalLink from './link';
 
-import {getWlkpArticles, getPolandArticles, getWorldArticles} from './utils/scraper';
+import oneClickHandler from './utils/one-click';
 
 const Utility = () => {
 	const [results, setResults] = useState(null);
-	const [formState, {select}] = useFormState();
 
-	const handleSubmit = async e => {
-		e.preventDefault();
-
-		const {values} = formState;
-
-		if (values.region === 'poland') {
-			await setResults('Pobieranie artykułów...');
-			const articles = await getPolandArticles();
-
-			const formatted = articles.map(e => (
-				<div key={e.title} style={{width: '80%'}}>
-					<h3>{e.title}</h3>
-					<br/>
-					<p>{e.description.replace(/czytaj|dalej|»/g, '')} <ExternalLink href={e.url}>[CZYTAJ DALEJ]</ExternalLink></p>
-					<br/>
-					<hr/>
-				</div>
-			));
-
-			setResults(
-				<>
-					{formatted}
-					<br/>
-					<footer>
-						<p>Źródło powyższych wiadomości: <ExternalLink href="https://www.tvn24.pl/wiadomosci-ze-swiata,2">TVN24</ExternalLink></p>
-						<b>Prawa autorskie do artykułów należą do ich autorów</b>
-					</footer>
-				</>
-			);
-		} else if (values.region === 'world') {
-			await setResults('Pobieranie artykułów...');
-			const articles = await getWorldArticles();
-
-			const formatted = articles.map(e => (
-				<div key={e.title} style={{width: '80%'}}>
-					<h3>{e.title}</h3>
-					<br/>
-					<p>{e.description.replace(/czytaj|dalej|»/g, '')} <ExternalLink href={e.url}>[CZYTAJ DALEJ]</ExternalLink></p>
-					<br/>
-					<hr/>
-				</div>
-			));
-
-			setResults(
-				<>
-					{formatted}
-					<br/>
-					<footer>
-						<p>Źródło powyższych wiadomości: <ExternalLink href="https://www.tvn24.pl/wiadomosci-ze-swiata,2">TVN24</ExternalLink></p>
-						<b>Prawa autorskie do artykułów należą do ich autorów</b>
-					</footer>
-				</>
-			);
-		} else {
-			await setResults('Pobieranie artykułów...');
-			const articles = await getWlkpArticles();
-
-			const formatted = articles.map(e => (
-				<div key={e.title} style={{width: '80%'}}>
-					<h3>{e.title}</h3>
-					<br/>
-					<p>{e.description} <ExternalLink href={e.url}>[CZYTAJ DALEJ]</ExternalLink></p>
-					<br/>
-					<hr/>
-				</div>
-			));
-
-			setResults(
-				<>
-					{formatted}
-					<br/>
-					<footer>
-						<p>Źródło powyższych wiadomości: <ExternalLink href="https://gloswielkopolski.pl/wiadomosci/wielkopolska/">Głos Wielkopolski</ExternalLink></p>
-						<b>Prawa autorskie do artykułów należą do ich autorów</b>
-					</footer>
-				</>
-			);
+	const handleSubmit = async () => {
+		if (!results) {
+			setResults('Generowanie prasówki...');
 		}
+
+		const result = await oneClickHandler();
+
+		setResults(
+			<>
+				{result}
+			</>
+		);
 	};
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
-				<label>
-        Region
-					<br/>
-					<Select required {...select('region')}>
-						<option value="">Wybierz</option>
-						<option value="wielkopolska">Wielkopolska</option>
-						<option value="poland">Polska</option>
-						<option value="world">Świat</option>
-					</Select>
-				</label>
-				<br/>
-				<Button type="submit">
-					Generuj
-				</Button>
-				<br/>
-				<br/>
-				{results}
-			</form>
+			<Button type="submit" onClick={handleSubmit}>
+				{results === null ? 'Wygeneruj prasówkę (beta)' : 'Wygeneruj ponownie'}
+			</Button>
+			<br/>
+			<br/>
+			{results}
 		</>
 	);
 };
