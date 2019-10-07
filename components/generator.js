89f9a6193142ba4d462b2498373c162340a746de
Antoni Kepinski <xxczaki@pm.me>
2019-10-07 23:39:14 +0200

    Use new loading animation & disable button when the generator is running

--- components/generator.js
+++ components/generator.js
@@ -1,29 +1,29 @@
 import React, {useState} from 'react';
 
 import Button from './button';
+import Loading from './loading';
 
 import oneClickHandler from './utils/one-click';
 
 const Utility = () => {
 	const [results, setResults] = useState(null);
+	const [clicked, setClicked] = useState(false);
 
 	const handleSubmit = async () => {
 		if (!results) {
-			setResults('Generowanie prasówki...');
+			setClicked(true);
+			setResults(<Loading/>);
 		}
 
 		const result = await oneClickHandler();
 
-		setResults(
-			<>
-				{result}
-			</>
-		);
+		setClicked(false);
+		setResults(result);
 	};
 
 	return (
 		<>
-			<Button type="submit" onClick={handleSubmit}>
+			<Button disabled={clicked} type="submit" onClick={handleSubmit}>
 				{results === null ? 'Wygeneruj prasówkę (beta)' : 'Wygeneruj ponownie'}
 			</Button>
 			<br/>
