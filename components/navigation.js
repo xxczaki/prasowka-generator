5b7c1bd8377bc9da285dacf96c61d0033295952b
Antoni Kepinski <xxczaki@pm.me>
2019-10-28 22:07:47 +0100

    Make navigation look better on mobile devices

--- components/navigation.js
+++ components/navigation.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import {Breakpoint, BreakpointProvider} from 'react-socks';
 
 import ExternalLink from './link';
 
-const Navigation = () => <p style={{margin: '2px', paddingBottom: '1.5em', paddingTop: '1em'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/faq"><ExternalLink>[FAQ]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></p>;
+const Navigation = () => (
+	<BreakpointProvider>
+		<Breakpoint small down>
+			<ul>
+				<li><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link></li>
+				<li><Link scroll={false} href="/generator"><ExternalLink>[Generator]</ExternalLink></Link></li>
+				<li><Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></li>
+			</ul>
+		</Breakpoint>
+		<Breakpoint large up>
+			<p style={{margin: '2px', paddingBottom: '1.5em', paddingTop: '1em'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/generator"><ExternalLink>[Generator]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></p>
+		</Breakpoint>
+	</BreakpointProvider>
+);
 
 export default Navigation;
