7d85579bf2054d2d3d8a6c45d786b30ec8dc3d2b
Antoni Kepinski <xxczaki@pm.me>
2019-10-28 22:16:26 +0100

    Default menu

--- components/navigation.js
+++ components/navigation.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import {Breakpoint, BreakpointProvider} from 'react-socks';
 
 import ExternalLink from './link';
 
-const Navigation = () => (
-	<BreakpointProvider>
-		<Breakpoint small down>
-			<ul>
-				<li><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link></li>
-				<li><Link scroll={false} href="/generator"><ExternalLink>[Generator]</ExternalLink></Link></li>
-				<li><Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></li>
-			</ul>
-		</Breakpoint>
-		<Breakpoint large up>
-			<p style={{margin: '2px', paddingBottom: '1.5em', paddingTop: '1em'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/generator"><ExternalLink>[Generator]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></p>
-		</Breakpoint>
-	</BreakpointProvider>
-);
+const Navigation = () => <p style={{margin: '2px', paddingBottom: '1.5em', paddingTop: '1em'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/generator"><ExternalLink>[Generator]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[Informacje]</ExternalLink></Link></p>;
 
 export default Navigation;
