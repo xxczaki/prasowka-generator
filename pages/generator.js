85b9d7039240cededd1fde0c9b3401a37d578d94
Antoni Kepinski <xxczaki@pm.me>
2019-10-28 22:09:33 +0100

    Change default page

--- pages/generator.js
+++ pages/generator.js
@@ -4,15 +4,15 @@ import Container from '../components/container';
 import Header from '../components/header';
 import Description from '../components/description';
 import Navigation from '../components/navigation';
-import Search from '../components/search';
+import Instant from '../components/instant';
 
 const Index = () => {
 	return (
 		<Container>
 			<Header>Prasówka Generator</Header>
-			<Description>Wyszukaj wiadomości do prasówki!</Description>
+			<Description>Wygeneruj prasówkę jednym kliknięciem!</Description>
 			<Navigation/>
-			<Search/>
+			<Instant/>
 		</Container>
 	);
 };
